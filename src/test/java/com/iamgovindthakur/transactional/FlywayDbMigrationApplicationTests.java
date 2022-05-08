7968de1292df27fe62d71cb90c7737ac14a7f782
iamgovindthakur <iamgovindthakur@gmail.com>
2022-05-08 13:59:31 +0530

    flyway-db migration demo

---
+++ src/test/java/com/iamgovindthakur/transactional/FlywayDbMigrationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.iamgovindthakur.transactional;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlywayDbMigrationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
